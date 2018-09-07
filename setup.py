48bcc3ec3cf1efc5ad945ce3ff0f521bf079ca14
Max Mahlke <m_mahlke@yahoo.de>
2018-09-07 17:13:26 +0200

    Fixed dependencies

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ from os import path
 
 setup(
     name='ssos',
-    version='0.9.dev',
+    version='0.9.dev0',
     description='ssos Pipeline',
     url='https://github.com/maxmahlke/ssos',
     author='Max Mahlke',
     author_email='max.mahlke@cab.inta-csic.es',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    setup_requires=packages,
+    install_requires = packages,
     entry_points={
             'console_scripts': ['ssos = ssos.__main__:main']
             }
