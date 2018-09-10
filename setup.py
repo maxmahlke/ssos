596e5ef5154d08517c1e8b9b84af14717d71f9e4
Max Mahlke <m_mahlke@yahoo.de>
2018-09-10 10:15:12 +0200

    Python3 required

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 from setuptools import setup, find_packages
 from os import path
 
+with open("README.md", 'r') as rm:
+    long_description = rm.read()
+
 setup(
     name='ssos',
     version='0.9.dev0',
@@ -9,10 +12,13 @@ setup(
     url='https://github.com/maxmahlke/ssos',
     author='Max Mahlke',
     author_email='max.mahlke@cab.inta-csic.es',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     setup_requires=['astropy', 'pandas', 'numpy', 'scipy', 'statsmodels'],
     install_requires = ['astropy', 'pandas', 'numpy', 'scipy', 'statsmodels'],
     entry_points={
             'console_scripts': ['ssos = ssos.__main__:main']
-            }
+            },
+    python_requires='>=3'
     )
