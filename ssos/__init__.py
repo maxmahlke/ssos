a92630cb666baca24027ee1f9fc2e7130296577d
Max Mahlke <m_mahlke@yahoo.de>
2018-09-11 14:49:39 +0200

    Version

--- ssos/__init__.py
+++ ssos/__init__.py
@@ -3,15 +3,15 @@ version = '1.0.1'
 
 
 GREETING = '''
-       *       +      ssos pipeline 1.0.1
+       *       +      ssos pipeline %s
    +        x
         '             https://sso-pipeline.readthedocs.io
 *   +     _.::'   |
-        (_.'    - o - ssos --default : Copy default settings to CWD
+        :_.'    - o - ssos --default : Copy default settings to CWD
 |                 |   ssos --help    : Print detailed help
 O -     *        '    ssos [-PARAMETER] [-VALUE] path/to/images
 |
-          _.::'       max.mahlke(at)cab.inta-csic.es
-  +     (_.'
+        _..:'         max.mahlke(at)cab.inta-csic.es
+  +   (_.:
 
-        '''
+        ''' % version
