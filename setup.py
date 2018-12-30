19475da5b8a716e2ba5efb116240e0dfc51cd042
Max Mahlke <m_mahlke@yahoo.de>
2018-12-30 18:43:55 +0100

    Header keywords read in more reliably

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as rm:
 
 setup(
     name = 'ssos',
-    version = '1.1.7',
+    version = '1.1.8',
     description = 'The ssos Pipeline - Identiciation of Solar System Objects in imaging surveys',
     url = 'https://github.com/maxmahlke/ssos',
     author = 'Max Mahlke',
